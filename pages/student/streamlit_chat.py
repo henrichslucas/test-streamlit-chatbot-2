a1b81fa79cdb536f067ea8d1690382997a2f0fe0
Lucas Chagas Henrichs <lucas.henrichs@e-core.com>
2024-11-06 21:29:13 -0300

    changed ai model, removed groq llm, updated prompt, added stop condition, added welcome message, updated rag retrieving method

--- pages/student/streamlit_chat.py
+++ pages/student/streamlit_chat.py
@@ -2,53 +2,54 @@ import streamlit as st
 from groq import Groq
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
-from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.embeddings import CohereEmbeddings
 
 pc = Pinecone(api_key=st.secrets.PINECONE_API_KEY)
 embeddings = CohereEmbeddings(model="embed-english-v3.0", user_agent="langchain")
 
-llm = ChatGroq(
-        model="mixtral-8x7b-32768",
-        temperature=0,
-        max_tokens=None,
-        timeout=None,
-        max_retries=1,
-    )
 
 def set_model():
     client = Groq(api_key=st.secrets.GROQ_API_KEY)
     
     # Set a default model
     if "ai_model" not in st.session_state:
-        st.session_state["ai_model"] = "llama-3.1-8b-instant"
+        st.session_state["ai_model"] = "mixtral-8x7b-32768"
 
     return client
+
+
+def sort_question_answer(data):
+    full_data = []
+
+    for d in data:
+        full_data.append(str(d))
+  
+    return full_data
 
 def response_generator(client, question, data):
     context = []
 
+    template = sort_question_answer(data)
+    #print(template)
+
     for m in st.session_state.messages:
         context.append({"role": m["role"], "content": m["content"]})
-
-    if data:    
-        data = data[0].metadata.get("resposta")
 
     stream = client.chat.completions.create(
         stream = True,
         model = st.session_state["ai_model"],
-        temperature=0.3,
-        top_p=0.5,
+        temperature=0.1,
+        top_p=0.2,
         messages = [
             {
-                "role": "system", "content": f"Você é um assistente escolar com o objetivo de ajudar alunos a sanarem suas dúvidas de forma simples e ágil, com base na resposta providenciada pelo professor. Responda exclusivamente em português brasileiro e use no máximo duas frases para responder. Utilize o contexto da conversa para recapitular perguntas já feitas. {context}. A resposta do professor está a seguir: {data}. Caso esteja vazio, responda com base no seu conhecimento."
+                "role": "system", "content": f"Você é um assistente escolar com o objetivo de ajudar alunos a sanarem suas dúvidas de forma simples e ágil, com base na resposta providenciada pelo professor. A resposta SEMPRE deve ser colocada entre as tags <response> </response>'. Responda exclusivamente em português brasileiro e NUNCA PASSE DE uma frase na resposta. Todo o conteudo corrigido pelo professor está a seguir: {template}. Voce tambem pode utilizar o historico da conversa para responder perguntas, desde que elas estejam de acordo com o conteudo corrigido. Aqui esta o historico:{context}. Nao se esqueca de ser educado e prestativo com o aluno, e nunca adicione comentarios adicionais. Nunca escreva as tags <response>, apenas o que estiver entre elas. Caso o conteudo corrigido esteja vazio, responda com 'Desculpe, o repositório esta sem conteudos atualmente, peça para seu professor adicionar algum gabarito!'"
             },
             {
                 "role": "user",
                 "content": f"{question}"
             },
-        ]
+        ],
+        stop="</response>",
     )
     return stream
     
@@ -58,12 +59,12 @@ def new_chat():
     st.session_state.assistant_messages = []
 
 def init_chat_session():
-    if "messages" not in st.session_state:
-        new_chat()
 
-        content = "Ola, eu sou o assistente escolar do Lucas Henrichs. Pergunte-me algo!"
-        st.session_state.messages.append({"role": "assistant", "content": content}) #adicionando no histórico de mensagens
-        st.session_state.assistant_messages.append({"role": "assistant", "content": content}) #adicionando no histórico de mensagens do bot
+    new_chat()
+
+    content = "Ola, eu sou o SaberIA. Pergunte-me algo!"
+    st.session_state.messages.append({"role": "assistant", "content": content}) #adicionando no histórico de mensagens
+    st.session_state.assistant_messages.append({"role": "assistant", "content": content}) #adicionando no histórico de mensagens do bot
 
 def display_chat_history():
     # Display chat messages from history on app rerun
@@ -92,7 +93,7 @@ def handle_page():
     with st.sidebar:
         st.markdown("## 🧠 Escolha a disciplina 🧠")
         st.selectbox("Disciplinas", ["História"])
-        st.button(("Iniciar novo chat"), on_click=new_chat)
+        st.button(("Iniciar novo chat"), on_click=init_chat_session)
 
 def write_response(client,data,question):
 
@@ -100,15 +101,20 @@ def write_response(client,data,question):
     for chunk in stream:
         content = chunk.choices[0].delta.content
         if content is not None:
+            content = content.replace("<response>", "")
             yield content
-        
 
 def get_chatbot_response(client):   
     docsearch = PineconeVectorStore(index_name=st.session_state.index_name, embedding=embeddings)
 
     question = st.session_state.user_messages[-1]["content"]
 
-    data = docsearch.search(question,search_type='similarity_score_threshold',k=1, score_threshold=0.8)
+    retriever = docsearch.as_retriever(
+        search_type="mmr",
+        search_kwargs={'k': 1000, 'lambda_mult': 0.25}
+    )
+    
+    data = retriever.invoke(question)
 
     with st.chat_message("assistant"):
         ai_response =  st.write_stream(write_response(client,data,question))
